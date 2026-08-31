b84bcd3c7fd3ace93242d5f7676cda8884b041cc
Hari Krishna Kancharla <162476937+harihkk@users.noreply.github.com>
2026-08-31 17:19:21 -0400

    Include the number and join string helpers in the subscriber

--- native/seoul/browser/adblock/ad_block_catalogue_subscriber.cc
+++ native/seoul/browser/adblock/ad_block_catalogue_subscriber.cc
@@ -8,6 +8,8 @@
 
 #include "base/functional/bind.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "url/gurl.h"
 
 namespace seoul::adblock {
