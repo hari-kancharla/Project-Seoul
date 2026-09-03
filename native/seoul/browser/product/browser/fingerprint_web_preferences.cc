5e977e623b0865d45f2648ab2b654d9894c4f07b
Hari Krishna Kancharla <162476937+harihkk@users.noreply.github.com>
2026-09-03 14:49:00 -0400

    Include WebContentsUserData for the taint bookkeeping

--- native/seoul/browser/product/browser/fingerprint_web_preferences.cc
+++ native/seoul/browser/product/browser/fingerprint_web_preferences.cc
@@ -6,6 +6,7 @@
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_user_data.h"
 #include "seoul/browser/adblock/ad_block_request.h"
 #include "seoul/browser/adblock/ad_block_service.h"
 #include "seoul/browser/adblock/ad_block_service_factory.h"
